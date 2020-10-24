c0ca67247c81dfe2076e9920d000f79fc403a376
Dylan <dylanabozarth@gmail.com>
2020-10-23 21:10:54 -0700

    downloading

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+namespace C__thing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        WebClient webClient = new WebClient();
+    
+webClient.DownloadFile("http://ringba-test-html.s3-website-us-west-1.amazonaws.com/TestQuestions/output.txt", "output.txt" );
+       
+         }
+    }
+}
